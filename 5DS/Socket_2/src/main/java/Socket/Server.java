0e98c2d83f5c9a34f70f3f24dbb3aa869f60a164
loris.martini <martini.loloris@gmail.com>
2024-11-04 08:25:14 +0100

    sviluppo

---
+++ 5DS/Socket_2/src/main/java/Socket/Server.java
@@ -0,0 +1,62 @@
+package Socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private ServerSocket server = null;
+    private Socket client = null;
+    private String stringa = null;
+    private DataInputStream dalClient;
+    private DataOutputStream versoClient;
+
+    public Socket attendi() {
+        try {
+            System.out.println("SERVER partito");
+            server = new ServerSocket(6789);
+            client = server.accept();
+            System.out.println("CLIENT CONNESSO \n");
+            server.close();
+            dalClient = new DataInputStream(client.getInputStream());
+            versoClient = new DataOutputStream(client.getOutputStream());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istanza del server !");
+            System.exit(1);
+        }
+        return client;
+    }
+
+    public void comunica() {
+        while (true) {
+            try {
+                System.out.println("...attesa...\n");
+                stringa = dalClient.readUTF();
+                System.out.println("RICEVO: " + stringa);
+                if (stringa.equalsIgnoreCase("chiudi")) {
+                    break;
+                }
+                System.out.println("RISPONDO: " + stringa + '\n');
+                versoClient.writeUTF(stringa);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Errore");
+            }
+        }
+        try {
+            System.out.println("CHIUDO");
+            client.close();
+        } catch (IOException ex) {
+        }
+    }
+
+    public static void main(String[] args) {
+        Server S = new Server();
+        S.attendi();
+        S.comunica();
+    }
+}
